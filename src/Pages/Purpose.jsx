e08932cd4d08d8b07f444f90b05caf1eab5711fe
Oluwadamilare Babalola <oladamilare10@gmail.com>
2024-10-03 10:38:36 -0700

    Update Purpose.jsx

--- src/Pages/Purpose.jsx
+++ src/Pages/Purpose.jsx
@@ -75,7 +75,7 @@ const Purpose = () => {
           <div className="-ml-12 -mt-12 p-12 lg:sticky lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden">
             <img
               className="w-[48rem] max-w-none rounded-xl bg-gray-900 shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem]"
-              src="https://th.bing.com/th?id=OIF.fLq%2f%2fhdj8mdIpGjBXzgcLA&pid=ImgDet&rs=1"
+              src="https://www.demandwealth.com/wp-content/uploads/2021/06/AdobeStock_180098018-1024x683.jpeg"
               alt=""
             />
           </div>
