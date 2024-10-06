cf4e4e3f926fb7b13012e03e4fcad7974fa1722b
Oluwadamilare Babalola <oladamilare10@gmail.com>
2024-10-06 06:57:05 -0700

    charity

--- src/Components/ContactForm.jsx
+++ src/Components/ContactForm.jsx
@@ -95,7 +95,7 @@ const ContactForm = () => {
 
   return (
     <>
-      <div className="isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
+      <div className="bg-white px-6 py-24 sm:py-32 lg:px-8">
         <div
             aria-hidden="true"
             className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]"
